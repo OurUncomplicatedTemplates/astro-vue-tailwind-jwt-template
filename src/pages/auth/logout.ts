2279f448a7c0f66b3fdaeb1f973ed7c4ffb5c1c6
Kristian Binau <kristian@binau.me>
2023-08-30 22:25:04 +0200

    Refactor folder structure and imports

--- src/pages/auth/logout.ts
+++ src/pages/auth/logout.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from 'astro';
-import { url } from '../../lib/data.js';
+import { url } from '../../lib/helpers';
 
 export const GET: APIRoute = ({ redirect }) =>  {
 	// Send request to backend to logout
