3624ce1086f01ff213feb731459ccef61d40842e
Kristian Binau <kristian@binau.me>
2023-09-04 20:00:52 +0200

    Try to make Tauri Build work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,36 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from 'vite';
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 import vercel from '@astrojs/vercel/serverless';
+import node from "@astrojs/node";
 
-const { PUBLIC_SITE_URL, PUBLIC_SITE_PORT } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+const {
+	PUBLIC_SITE_URL,
+	PUBLIC_SITE_PORT,
+	TAURI_BUILD
+} = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 const SITE = PUBLIC_SITE_PORT ? `${PUBLIC_SITE_URL}:${PUBLIC_SITE_PORT}` : PUBLIC_SITE_URL;
 const PORT = Number(PUBLIC_SITE_PORT) || 4321;
+
+let adapter = vercel();
+if (TAURI_BUILD) {
+	adapter = node({
+		mode: "standalone"
+	});
+}
 
 // https://astro.build/config
 export default defineConfig({
 	site: SITE,
 	output: 'server',
 	server: {
-		port: PORT,
+		port: PORT
 	},
 	integrations: [sitemap(), tailwind(), vue()],
-	adapter: vercel({
-		functionPerRoute: false,
-	}),
+	adapter: adapter,
 	vite: {
 		server: {
 			watch: {
--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
 	readonly PUBLIC_SITE_URL: string;
 	readonly PUBLIC_SITE_PORT: number | undefined;
 	readonly PUBLIC_API_URL: string;
+	readonly TAURI_BUILD: boolean | undefined;
 }
 
 interface ImportMeta {
