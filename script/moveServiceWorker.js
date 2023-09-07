651c2c26c9238a582fbb4be4819a295682b81fbe
Kristian Binau <kristian@binau.me>
2023-09-07 15:21:06 +0200

    #1 - Add script to update serviceWorker at every build

---
+++ script/moveServiceWorker.js
@@ -0,0 +1,13 @@
+import fs from 'fs'
+import 'dotenv/config'
+
+const path = './public/serviceWorker.js'
+
+const API_URL = process.env.PUBLIC_API_URL || ''
+
+fs.readFile(path, { encoding: 'utf8' }, function (err, data) {
+	var formatted = data.replace(/^const API_URL.*/g, `const API_URL = '${API_URL}';`);
+	fs.writeFile(path, formatted, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+});
