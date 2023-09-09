822957e20c95a0ec71448fdd87e907d5c2cd599b
Kristian Binau <kristian@binau.me>
2023-09-09 19:53:01 +0200

    #3 - Everything seems to work, build finishes & is installable but can't find index.html

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{
+  api::process::{Command, CommandEvent},
+  Manager,
+};
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+      tauri::async_runtime::spawn(async move {
+        let (mut rx, mut child) = Command::new_sidecar("app")
+          .expect("failed to setup `app` sidecar")
+          .spawn()
+          .expect("Failed to spawn packaged node");
+
+        let mut i = 0;
+        while let Some(event) = rx.recv().await {
+          if let CommandEvent::Stdout(line) = event {
+            window
+              .emit("message", Some(format!("'{}'", line)))
+              .expect("failed to emit event");
+            i += 1;
+            if i == 4 {
+              child.write("message from Rust\n".as_bytes()).unwrap();
+              i = 0;
+            }
+          }
+        }
+      });
+
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
